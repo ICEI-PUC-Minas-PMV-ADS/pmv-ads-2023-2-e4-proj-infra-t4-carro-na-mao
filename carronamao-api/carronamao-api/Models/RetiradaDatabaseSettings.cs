50de8b783efc0469ab01b30e9e8589d0a8052c5a
Matheus Sampaio <matheuslemos98@gmail.com>
2023-09-25 16:01:28 -0300

    Insere entidade retirada e insere endpoints desta entidade

---
+++ carronamao-api/carronamao-api/Models/RetiradaDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace carronamao_api.Models
+{
+    public class RetiradaDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null;
+        public string DatabaseName { get; set; } = null;
+        public string RetiradaCollectionName { get; set; } = null;
+    }
+}

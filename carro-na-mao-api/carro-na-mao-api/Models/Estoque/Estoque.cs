d4fc239ca9ee18b4feb39a4ef1128e3a2f39b6ca
Matheus Sampaio <matheuslemos98@gmail.com>
2023-09-26 11:21:34 -0300

    Realiza requisitos 05 e 07 para ajustar dev

---
+++ carro-na-mao-api/carro-na-mao-api/Models/Estoque/Estoque.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace carro_na_mao_api.Models.Estoque
+{
+    public class Estoque
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? id_estoque { get; set; }
+
+        [BsonElement("id_veiculo")]
+        public int id_veiculo { get; set; }
+
+        [BsonElement("cor_veiculo")]
+        public string? cor_veiculo { get; set; } = null;
+
+        [BsonElement("quantidade")]
+        public int quantidade { get; set; }
+    }
+}

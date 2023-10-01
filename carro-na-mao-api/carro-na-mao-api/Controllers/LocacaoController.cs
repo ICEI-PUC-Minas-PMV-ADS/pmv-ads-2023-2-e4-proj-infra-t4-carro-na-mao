1ead102c52eafff06ba85b5cc5aa5ff10a75b741
mthszf99 <101369477+mthszf99@users.noreply.github.com>
2023-10-01 14:01:15 -0300

    Authorize no locacao

--- carro-na-mao-api/carro-na-mao-api/Controllers/LocacaoController.cs
+++ carro-na-mao-api/carro-na-mao-api/Controllers/LocacaoController.cs
@@ -1,11 +1,13 @@
 ﻿
 using carro_na_mao_api.Models.Reservas;
 using carro_na_mao_api.Service;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace carro_na_mao_api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class LocacaoController : ControllerBase

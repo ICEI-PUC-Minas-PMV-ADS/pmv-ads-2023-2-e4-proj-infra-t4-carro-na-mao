eec6e5cd3a9995199c5538d331ca0a053e4e1b80
rogerpucmg <101529285+rogerpucmg@users.noreply.github.com>
2023-10-02 19:53:52 -0300

    api-notificacao
    Envio da api

---
+++ carro-na-mao-api/carro-na-mao-api/Models/Notificacao/Notificacao.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace carro_na_mao_api.Models.Notificacao
+{
+    public class Notificacao
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id_notificacao { get; set; }
+
+        [BsonElement("Data")]
+        public string? Data { get; set; } = null;
+
+        [BsonElement("Assunto")]
+        public int Assunto { get; set; }
+
+        [BsonElement("Corpo Mensagem")]
+        public int Corpomensagem { get; set; }
+
+    }
+}
---
+++ carro-na-mao-api/carro-na-mao-api/Service/NotificacaoService.cs
@@ -0,0 +1,40 @@
+﻿using carro_na_mao_api.Models.Notificacao;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace carro_na_mao_api.Service
+{
+    public class NotificacaoService
+    {
+
+        private readonly IMongoCollection<Notificacao> _notificacaoCollection;
+
+        public NotificacaoService(IOptions<NotificacaoDatabaseSettings> notificacaoService)
+        {
+            var mongoClient = new MongoClient(notificacaoService.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(notificacaoService.Value.DatabaseName);
+
+            _notificacaoCollection = mongoDatabase.GetCollection<Notificacao>
+                (notificacaoService.Value.NotificacaoCollectionName);
+
+        }
+
+        public async Task<List<Notificacao>> getAsync() =>
+           await _notificacaoCollection.Find(x => true).ToListAsync();
+        public async Task<Notificacao> getAsyncId(string id) =>
+            await _notificacaoCollection.Find(x => x.Id_notificacao == id).SingleOrDefaultAsync();
+        public async Task CreateAsync(Notificacao notificacao) =>
+           await _notificacaoCollection.InsertOneAsync(notificacao);
+        public async Task UpdateAsync(string id, Notificacao notificacao) =>
+           await _notificacaoCollection.ReplaceOneAsync(x => x.Id_notificacao == id, notificacao);
+        public async Task DeleteAsync(string id) =>
+           await _notificacaoCollection.DeleteOneAsync(x => x.Id_notificacao == id);
+
+       
+       /// internal Task<Notificacao> findByValue(int vl_notificacao)
+       /// {
+          ///  throw new NotImplementedException();
+        ///} 
+
+    }
+}

26a113de531e44503a857e4341efb517854f3513
DanielMarkline <592075@sga.pucminas.br>
2023-10-05 19:26:24 -0300

    update vistoria fix

--- carro-na-mao-api/carro-na-mao-api/Models/Vistoria/Vistoria.cs
+++ carro-na-mao-api/carro-na-mao-api/Models/Vistoria/Vistoria.cs
@@ -10,21 +10,30 @@ namespace carro_na_mao_api.Models.Vistoria
         public string? id_vistoria { get; set; }
 
         [BsonElement("id_veiculo")]
-        public int id_veiculo { get; set; }
+        public int? id_veiculo { get; set; } = null;
 
+<<<<<<< Updated upstream
         [BsonElement("data")]
         public string? data { get; set; } = null;
+=======
+        [BsonElement("data_vistoria")]
+        public DateTime? data_vistoria { get; set; } = null;
+>>>>>>> Stashed changes
 
         [BsonElement("tipo")]
-        public int tipo { get; set; }
+        public int? tipo { get; set; } = null;
 
         [BsonElement("descricao")]
-        public int descricao { get; set; }
+        public string? descricao { get; set; } = null;
 
         [BsonElement("observacoes")]
-        public int observacoes { get; set; }
+        public string? observacoes { get; set; } = null;
 
         [BsonElement("cria_ordem_manut")]
+<<<<<<< Updated upstream
         public int cria_ordem_manut { get; set; }
+=======
+        public bool? cria_ordem_manut { get; set; } = null;
+>>>>>>> Stashed changes
     }
 }

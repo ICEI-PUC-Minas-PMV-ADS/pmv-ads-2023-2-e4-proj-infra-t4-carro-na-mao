2e9a8c21d14a1b8773d2648475290ed9802210b9
rogerpucmg <101529285+rogerpucmg@users.noreply.github.com>
2023-10-05 09:59:25 -0300

    api notification
    api notificacao

---
+++ carro-na-mao-api/carro-na-mao-api/Models/Notificacao/Notificacao.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace carro_na_mao_api.Models.Notificacao
+{
+    public class Notificacao
+    {
+        [BsonId]
+        public string? id_notificacao { get; set; }
+
+        [BsonElement("Data")]
+        public DateTime Data { get; set; }
+
+        [BsonElement("Assunto")]
+        public string? Assunto { get; set; }
+
+        [BsonElement("Corpo Mensagem")]
+        public string? Corpomensagem { get; set; }
+
+    }
+}

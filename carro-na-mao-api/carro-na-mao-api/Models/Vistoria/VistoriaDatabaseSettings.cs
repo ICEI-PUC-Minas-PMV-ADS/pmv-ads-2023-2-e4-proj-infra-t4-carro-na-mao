d76873a2302336207df7afde0866cfc60054ca56
DanielMarkline <592075@sga.pucminas.br>
2023-10-05 22:37:11 -0300

    Limpeza e teste
    Limpeza e teste

--- carro-na-mao-api/carro-na-mao-api/Models/Vistoria/VistoriaDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace carro_na_mao_api.Models.Vistoria
-{
-    public class VistoriaDatabaseSettings
-    {
-        public string ConnectionString { get; set; } = null;
-        public string DatabaseName { get; set; } = null;
-        public string VistoriaCollectionName { get; set; } = null;
-    }
-}

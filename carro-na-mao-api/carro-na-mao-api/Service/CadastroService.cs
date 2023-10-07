6572de215e1c03d1bf6b568a4046ff6b62698617
Vitor Hugo <vitorhugoemail@gmail.com>
2023-10-06 22:00:59 -0300

    Add files via upload

--- carro-na-mao-api/carro-na-mao-api/Service/CadastroService.cs
+++ carro-na-mao-api/carro-na-mao-api/Service/CadastroService.cs
@@ -20,6 +20,9 @@ namespace carro_na_mao_api.Service
         public async Task<List<Cadastro>> GetAsync() =>
             await _cadastroCollection.Find(x => true).ToListAsync();
 
+        public async Task<Cadastro> GetAsyncEmail(string email) =>
+            await _cadastroCollection.Find(x => x.email == email).SingleOrDefaultAsync();
+
         public async Task CreateAsync(Cadastro cadastro) =>
             await _cadastroCollection.InsertOneAsync(cadastro);
 

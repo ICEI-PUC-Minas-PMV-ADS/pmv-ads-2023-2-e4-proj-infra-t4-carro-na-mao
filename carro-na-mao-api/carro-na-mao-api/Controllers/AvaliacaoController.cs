4dfd53e935aac05987f36ed586ced53a743460ef
Pedro Almeida <101743181+Pedroassis1707@users.noreply.github.com>
2023-10-06 21:50:31 -0300

    Resolvendo conflitos do avaliação

--- carro-na-mao-api/carro-na-mao-api/Controllers/AvaliacaoController.cs
+++ carro-na-mao-api/carro-na-mao-api/Controllers/AvaliacaoController.cs
@@ -1,4 +1,6 @@
 ﻿using carro_na_mao_api.Models.Avaliacao;
+using carro_na_mao_api.Models.Estoque;
+using carro_na_mao_api.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApiMongoDB.Services;
@@ -10,6 +12,7 @@ namespace carro_na_mao_api.Controllers
     public class AvaliacaoController : ControllerBase
     {
         private readonly AvaliacaoServices _avaliacaoServices;
+        private object avaliacao;
 
         public AvaliacaoController(AvaliacaoServices avaliacaoServices)
         {
@@ -20,12 +23,26 @@ namespace carro_na_mao_api.Controllers
         public async Task<List<Avaliacao>> GetAvaliacao()       
           => await _avaliacaoServices.GetAsync();
 
-        [HttpGet]
+        [HttpPost]
         public async Task<Avaliacao> PostAvaliacao(Avaliacao avaliacao)
         {
             await _avaliacaoServices.createAsync(avaliacao);
             return avaliacao;
         }
+        
+        [HttpDelete]
+        public async Task DeleteAvaliacao(int id_veiculo)
+        {
+            await _avaliacaoServices.DeleteAsync(id_veiculo);
+            
+        }
+        
+        [HttpPut]
+        public async Task<Avaliacao> UpdateAvaliacao(string id, Estoque estoque)
+        {
+            await _avaliacaoServices.UpdateAsync(id, avaliacao);
 
+            return (Avaliacao)avaliacao;
+        }
     }
 }

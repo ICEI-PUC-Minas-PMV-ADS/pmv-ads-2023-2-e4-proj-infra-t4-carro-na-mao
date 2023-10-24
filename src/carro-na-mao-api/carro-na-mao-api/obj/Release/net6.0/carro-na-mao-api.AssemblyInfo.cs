09ad1a3f24b3059a46bd66d5441f5a9bb59a5e3e
mthszf99 <101369477+mthszf99@users.noreply.github.com>
2023-10-24 19:18:34 -0300

    mudanca api locacao

--- src/carro-na-mao-api/carro-na-mao-api/obj/Release/net6.0/carro-na-mao-api.AssemblyInfo.cs
+++ src/carro-na-mao-api/carro-na-mao-api/obj/Release/net6.0/carro-na-mao-api.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("carro-na-mao-api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("carro-na-mao-api")]

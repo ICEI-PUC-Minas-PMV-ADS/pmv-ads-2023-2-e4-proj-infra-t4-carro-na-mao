9317ead1625b5ea823a64fd3feb489674182db98
mthszf99 <101369477+mthszf99@users.noreply.github.com>
2023-10-28 14:40:06 -0300

    arrumando api locacao

--- src/carro-na-mao-api/carro-na-mao-api/Service/LocacaoService.cs
+++ src/carro-na-mao-api/carro-na-mao-api/Service/LocacaoService.cs
@@ -20,7 +20,7 @@ namespace carro_na_mao_api.Service
             _locacaoCollection = mongoDatabase.GetCollection<Locacao>(locacaoService.Value.LocacaoCollectionName);
         }
 
-        public async Task<List<Locacao>> GetAsync() =>
+        public async Task<List<Locacao>>getAsync() =>
             await _locacaoCollection.Find(x => true).ToListAsync();
 
         public async Task<Locacao> getAsyncId(string id) =>
@@ -38,9 +38,6 @@ namespace carro_na_mao_api.Service
         public async Task DeleteAsync(string id) =>
             await _locacaoCollection.DeleteOneAsync(x => x.id_locacao == id);
 
-        internal Task<List<Locacao>> getAsync()
-        {
-            throw new NotImplementedException();
-        }
+    
     }
 }

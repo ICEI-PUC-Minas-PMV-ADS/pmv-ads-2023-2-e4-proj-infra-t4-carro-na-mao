7ea9117a62fd4abcf0f703b91a6062198cad2fbd
Matheus Sampaio <matheuslemos98@gmail.com>
2023-10-28 17:43:33 -0300

    Recriando tela de localizacao

--- src/carro-na-mao-front/src/pages/localizacao.js
+++ src/carro-na-mao-front/src/pages/localizacao.js
@@ -9,6 +9,9 @@ import '../estilos/localizacao.css'
 function Localizacao() {
     const navigate = useNavigate()
     const [token, setToken] = useState(null)
+    const [showDivs, setShowDivs] = useState(false); 
+    const [localizacaoData, setLocalizacaoData] = useState(null);
+
     useEffect(() => {
         async function fetchData() {
             try{
@@ -21,6 +24,28 @@ function Localizacao() {
         fetchData()
     }, []);
 
+    const findLocalizacao = () => {
+        const locacao = document.querySelector("#locacao").value;
+
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": 'Bearer ' + token
+        }
+
+        axios.get(`https://api-carronamao.azurewebsites.net/find-by-locacao?id_locacao=${locacao}`, { headers })
+            .then(response => {
+                console.log(response.status);
+                if (response.status === 200) {
+                    const codLocal = response.data.id_local;
+                    setLocalizacaoData(response.data);
+                    setShowDivs(true);
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return(
         <>
         <Menu/>
@@ -28,11 +53,21 @@ function Localizacao() {
             <div style={{ width: '25%', float: 'left'}}>
                 <labe>Insira o código da Locação</labe>
                 <br/>
-                <input type="number"/>
+                <input id="locacao" type="text" onChange={findLocalizacao}/>
             </div>
-            <div id="divMap">
-                <label id="textMap">Este é o local de retirada do seu veículo</label>
+            <div>
+                {showDivs && localizacaoData ? (
+                    <>
+                        <div>
+
+                        </div>
+                        <div id="divMap">
+                            <label id="textMap">Este é o local de retirada do seu veículo</label>
+                        </div>
+                    </>
+                ): null}
             </div>
+            
         </section>
         </>
     )

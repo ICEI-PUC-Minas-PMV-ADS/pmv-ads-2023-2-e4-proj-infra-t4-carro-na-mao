4bd0eba62a63c54888a08786b5d6bb6639c646d7
vitor hugo <vitorhugoemail@gmail.com>
2023-10-29 00:48:08 -0300

    finalizando documentação e estilo cadastro e login

--- src/carro-na-mao-front/src/pages/menu.js
+++ src/carro-na-mao-front/src/pages/menu.js
@@ -20,30 +20,18 @@ export function Menu (){
 
       <>
         <div id="menu">
-<<<<<<< HEAD
-        <Link to="/Home2">Home</Link>
-=======
         <Link to="/Home2">Home</Link>        
->>>>>>> 6d0c8f6688b9f45b53dcc76ac37fdcff07a94448
         <Link to="/Infracoes">Infrações</Link>
         <Link to="/Historico">Histórico</Link>
         <Link to="/Avaliacoes">Avaliações</Link>        
         <Link to="/Dashboard">Dashboard</Link>
         <Link to="/Locacao">Locacao</Link>
-<<<<<<< HEAD
-        <Link to="/Vistoria">Vistoria</Link>
-        <Link to="/Estoque">Estoque</Link>
-        <Link to="/Categoria">Categoria</Link>
-        <Link to="/Localização">Localização</Link>
-        <Link to="/Manutencao">Manutenção</Link>
-=======
         <Link to="/Vistoria">Cadastrar Vistoria</Link>
         <Link to="/Vistoria2">Consultar Vistorias</Link>
         <Link to="/Estoque">Estoque</Link>
         <Link to="/Categoria">Categoria</Link>     
         <Link to="/Localizacao">Localização</Link>   
         <Link to="/Manutencao">Manutencao</Link>
->>>>>>> 6d0c8f6688b9f45b53dcc76ac37fdcff07a94448
         <Link to="/Notificacoes">Notificações</Link>
         <select value={selectedOption} onChange={handleOptionChange}>
           <option>Adm</option>

5b9596b317c0c45220b47df53dd7203c079491db
Pedro Almeida <101743181+Pedroassis1707@users.noreply.github.com>
2023-11-03 22:55:14 -0300

    CSS Menu

--- src/carro-na-mao-front/src/pages/menu.js
+++ src/carro-na-mao-front/src/pages/menu.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 //imports da funcionalidade notificcacao
 import Apps from './Apps';
 
-export function Menu (){
+export function Menu() {
 
   const [selectedOption, setSelectedOption] = useState('');
   const navigate = useNavigate();
@@ -16,36 +16,43 @@ export function Menu (){
     navigate(`/${value}`); // Navegar para a página correspondente
   };
 
-     return (
+  return (
 
-      <>
-        <nav class= "drop-menu">
-          <ul>
+    <>
+      <nav class="drop-menu">
+        <ul>
           <li><a href="#">Menu</a>
-          <ul>
-          <li><Link to="/Home2">Home</Link></li>        
-          <li><Link to="/Dashboard">Dashboard</Link></li>
-          <li><Link to="/Locacao">Alugar</Link></li>
-          <li><Link to="/Vistoria">Cadastrar Vistoria</Link></li>
-          <li><Link to="/Vistoria2">Consultar Vistorias</Link></li>  
-          <li><Link to="/Localizacao">Localização</Link></li>   
-          <li><Link to="/Manutencao">Manutencao</Link></li>
-          <li><Link to="/Infracoes">Infrações</Link></li>
-          <li><Link to="/Historico">Histórico</Link></li>
-          <li><Link to="/Notificacoes">Notificações</Link></li>
-          </ul>
+            <ul>
+              <li><Link to="/Home2">Home</Link></li>
+              <li><Link to="/Dashboard">Dashboard</Link></li>
+              <li><Link to="/Locacao">Alugar</Link></li>
+              <li><Link to="/avaliacoes">Avaliações</Link></li>
+              <li><Link to="/Vistoria">Cadastrar Vistoria</Link></li>
+              <li><Link to="/Vistoria2">Consultar Vistorias</Link></li>
+              <li><Link to="/Localizacao">Localização</Link></li>
+              <li><Link to="/Manutencao">Manutencão</Link></li>
+              <li><Link to="/Infracoes">Infrações</Link></li>
+              <li><Link to="/Historico">Histórico</Link></li>
+              <li><Link to="/Notificacoes">Notificações</Link></li>
+            </ul>
           </li>
-          <li><select id="selectAdm" value={selectedOption} onChange={handleOptionChange}>
-            <option>Adm</option>
-            <option value="Estoque">Estoque</option>
-            <option value="Categoria">Categoria</option>
-          </select>
-          </li>
+          
           <Apps />
-          <li id="sairButton"><Link to="/">Sair</Link></li>
-          </ul>
-        </nav>
-       </>
 
-     );
+          <li id="sairButton"><Link to="/">Sair</Link></li>
+
+          <li id="selectAdm">
+            <a href="#">Adm</a>
+            
+            <ul>
+              <li><Link to="/Estoque">Estoque</Link></li>
+              <li><Link to="/Categoria">Categoria</Link></li>
+            </ul>
+
+          </li>
+        </ul>
+      </nav>
+    </>
+
+  );
 }

98787c3288c061fc01964d4e0201724dcc2f3bbd
mthszf99 <101369477+mthszf99@users.noreply.github.com>
2023-11-07 21:42:30 -0300

    arrumando alguns detalhes da pagina locacao e teste de css

--- src/carro-na-mao-front/src/pages/locacao.js
+++ src/carro-na-mao-front/src/pages/locacao.js
@@ -11,22 +11,35 @@ function Locacao() {
     const [diaria, setDiaria] = useState(0);
     const [total, setTotal] = useState(0);
     const [diffInDays, setDiffInDays] = useState(0);
+    const [reserva, setReservar] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [selectLocacao, setSelectLocacao] = useState(null);
+
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const jwtToken = await RecuperaToken();
                 setToken(jwtToken);
+
+                const response = await axios.get('https://api-carronamao.azurewebsites.net/api/Locacao', {
+                    headers: {
+                        "Authorization": 'Bearer ' + jwtToken
+                    }
+                });
+                setReservar(response.data)
+                setIsLoading(false);
+
             } catch (error) {
                 console.error('Erro ao recuperar token:', error);
             }
         }
-            fetchData();
-            }, []);
+        fetchData();
+    }, []);
 
-    
-   
-const reservar = () => {
+
+
+    const reservar = () => {
         const local = document.querySelector('#localRetirada').value
         const categoria = document.querySelector('#categoriaRetirada').value
         const modelo = document.querySelector('#modeloRetirada').value
@@ -38,39 +51,97 @@ const reservar = () => {
         const dataEntrega = document.querySelector("#dataEntrega").value
 
         const data = {
-                "id_local": local,
-                "id_categoria": categoria,
-                "modelo_veiculo": modelo,
-                "hora_retirada": horaRetirada,
-                "hora_entrega": horaEntrega,
-                "vl_categoria": valor,
-                "custos_ad": custosAd,
-                "data_retirada": dataRetirada,
-                "data_entrega": dataEntrega
-             }
+            "id_local": local,
+            "id_categoria": categoria,
+            "modelo_veiculo": modelo,
+            "hora_retirada": horaRetirada,
+            "hora_entrega": horaEntrega,
+            "vl_categoria": valor,
+            "custos_ad": custosAd,
+            "data_retirada": dataRetirada,
+            "data_entrega": dataEntrega
+        }
 
-             const headers ={
-                "Content-Type":"application/json",
-                "Authorization": 'Bearer ' + token
-               }
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": 'Bearer ' + token
+        }
 
-               axios.post('https://api-carronamao.azurewebsites.net/api/Locacao',data,{headers})
-              .then(response =>{
-                  console.log(response.status)
-                  if(response.status===200){
-                      alert('Reserva Cadastrada com sucesso')
-                     return navigate("/Locacao")
-                  }
+
+        if (selectLocacao && selectLocacao.id_locacao) {
+            axios.put(`https://api-carronamao.azurewebsites.net/api/Locacao?id=${selectLocacao.id_locacao}`, data, { headers })
+                .then(response => {
+                    console.log(response.status);
+                    if (response.status === 200) {
+                        alert("Solicitação de locação realizado com sucesso!");
+                        return navigate("/Locacao");
+                    }
+                })
+                .catch(error => {
+                    alert(error.status);
+                });
+        } else {
+            axios.post('https://api-carronamao.azurewebsites.net/api/Locacao', data, { headers })
+                .then(response => {
+                    console.log(response.status)
+                    if (response.status === 200) {
+                        alert('Reserva Cadastrada com sucesso')
+                        return navigate("/Locacao")
+                    }
                 }
                 ).catch(error => {
-                  alert(error.status)
-              })
-            }
+                    alert(error.status)
+                })
+        }
+    }
+
+    const excluir = () => {
+        if (selectLocacao && selectLocacao.id_locacao) {
+            axios.delete(`https://api-carronamao.azurewebsites.net/api/Locacao?id=${selectLocacao.id_locacao}`, {
+                headers: {
+                    "Authorization": 'Bearer ' + token
+                }
+            })
+                .then(response => {
+                    console.log(response.status);
+                    if (response.status === 200) {
+                        alert("Reserva excluída com sucesso!");
+                        return navigate("/Locacao");
+                    }
+                })
+                .catch(error => {
+                    alert(error);
+                });
+        }
+    }
+
+    const handleLocacaoClick = (reservar) => {
+        setSelectLocacao(reservar);
+        document.querySelector("#localRetirada").value = reservar.id_local;
+        document.querySelector("#categoriaRetirada").value = reservar.id_categoria;
+        document.querySelector("#modeloRetirada").value = reservar.modelo_veiculo;
+        document.querySelector("#horaRetirada").value = reservar.hora_retirada;
+        document.querySelector("#horaEntrega").value = reservar.hora_entrega;
+        document.querySelector("#valorCategoria").value = reservar.vl_categoria;
+        document.querySelector("#custosAd").value = reservar.custos_ad;
+        document.querySelector("#dataRetirada").value = reservar.data_retirada;
+        document.querySelector("#dataEntrega").value = reservar.data_entrega;
+    }
+
+    const limparCampos = () => {
+        document.querySelector("#localRetirada").value = ''
+        document.querySelector("#categoriaRetirada").value = ''
+        document.querySelector("#modeloRetirada").value = ''
+        document.querySelector("#horaRetirada").value = ''
+        document.querySelector("#horaEntrega").value = ''
+        document.querySelector("#valorCategoria").value = ''
+        document.querySelector("#custosAd").value = ''
+        document.querySelector("#dataRetirada").value = ''
+        document.querySelector("#dataEntrega").value = ''
+    }
 
 
-    
-           
-const calculateDateDiff = () => {
+    const calculateDateDiff = () => {
         const dataRetirada = new Date(document.querySelector("#dataRetirada").value);
         const dataEntrega = new Date(document.querySelector("#dataEntrega").value);
 
@@ -83,7 +154,7 @@ const calculateDateDiff = () => {
 
 
 
-const calculateDiaria = () => {
+    const calculateDiaria = () => {
         const valorCategoria = parseFloat(document.querySelector("#valorCategoria").value);
         const daysDifference = calculateDateDiff();
         const calculatedDiaria = valorCategoria * daysDifference;
@@ -91,87 +162,129 @@ const calculateDiaria = () => {
         return calculatedDiaria;
     };
 
-const calculateTotal = () => {
+    const calculateTotal = () => {
         const valorAdicional = parseFloat(document.querySelector("#custosAd").value);
         const valorDiario = calculateDiaria();
         const calculatedTotal = valorAdicional + valorDiario;
         setTotal(calculatedTotal);
     };
 
+    const handleModeloChange = () => {
+        const select = document.getElementById('modeloRetirada');
+        const input = document.getElementById('valorCategoria');
+    
+        const selectedValue = select.value;
+        const selectedOption = select.options[select.selectedIndex];
+    
+        if (selectedValue !== "Selecione um modelo") {
+            input.value = selectedOption.getAttribute('value');
+        } else {
+            input.value = '';
+        }
+    };
+
     return (
-         
         <>
-        <Menu/>
-        <h1>Locacao</h1>
-            <div id="tela">
+            <Menu />
+            <h1 id="titulo">Locacao</h1>
+
+            {isLoading ? (
+                <div className="loading-container">
+                    <div className="loading"></div>
+                </div>
+            ) : (
+                <>
+                    <div id="divLocacoes">
+                        <ul>
+                            {reserva.map(reservar => (
+                                <li key={reservar.id_locacao} onClick={() => handleLocacaoClick(reservar)}
+                                >{reservar.data_retirada}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </>
+            )}
+
             <section id="campos">
-        <label id="labLocal">Selecione o local para retirada</label>
-        <select id="localRetirada">
-            <option id="SelecLoc">Selecione um local</option>
-            <option value="001">Av. Afonso Pena, 1.000 - Centro - BH/MG</option>
-            <option value="002">Av. Antonio Carlos, 1.001 - Pampulha - BH/MG</option>
-        </select>
 
-        <label id="labCategoria">Selecione a categoria de veículos desejada</label>
-        <select id="categoriaRetirada">
-            <option id="selecCat">Selecione uma categoria</option>
-            <option value="SUV's">SUV's</option>
-            <option value="Sedans">Sedans</option>
-            <option value="Hatches">Hatches</option>
-            <option value="Premium">Premium</option>
-        </select>
 
-        <label id="labModelo">Selecione o modelo de veículo desejado</label>
-        <select id="modeloRetirada">
-            <option id="Selec">Selecione um modelo</option>
-            <option id="HatchGol" value="50">VW Gol</option>
-            <option id="HatchOnix" value="50">GM Onix</option>
-            <option id="HatchHB20" value="50">Hyundai HB20</option>
-            <option id="HatchKa" value="50">Ford Ka</option>
-            <option id="SedanPrisma" value="70">GM Prisma</option>
-            <option id="SedanHB20S" value="70">Hyundai HB20S</option>
-            <option id="PremiumCorolla" value="270">Toyota Corolla</option>
-            <option id="PremiumCivic" value="270">Honda Civic</option>
-            <option id="SuvTcross" value="200">VW T-Cross</option>
-            <option id="SuvRenegade" value="200">Jeep Renegade</option>
-        </select>
 
-        <label id="labHrRetirada">Selecione a hora da retirada</label>
-        <input type="time" id="horaRetirada"></input>
+                <label id="labLocal">Selecione o local para retirada</label>
+                <select id="localRetirada">
+                    <option id="SelecLoc">Selecione um local</option>
+                    <option value="001">Av. Afonso Pena, 1.000 - Centro - BH/MG</option>
+                    <option value="002">Av. Antonio Carlos, 1.001 - Pampulha - BH/MG</option>
+                </select>
 
-        <label id="labHrEntrega">Selecione a hora da entrega</label>
-        <input type="time" id="horaEntrega"></input>
+                <label id="labCategoria">Selecione a categoria de veículos desejada</label>
+                <select id="categoriaRetirada">
+                    <option id="selecCat">Selecione uma categoria</option>
+                    <option value="SUV's">SUV's</option>
+                    <option value="Sedans">Sedans</option>
+                    <option value="Hatches">Hatches</option>
+                    <option value="Premium">Premium</option>
+                </select>
 
-        <label id="labValor">Valor/dia</label>
-        <input type="number" id="valorCategoria"></input>
+                <label id="labModelo">Selecione o modelo de veículo desejado</label>
+                <select id="modeloRetirada" onChange={handleModeloChange}> 
+                    <option id="Selec">Selecione um modelo</option>
+                    <option id="HatchGol" value="50">VW Gol</option>
+                    <option id="HatchOnix" value="50">GM Onix</option>
+                    <option id="HatchHB20" value="50">Hyundai HB20</option>
+                    <option id="HatchKa" value="50">Ford Ka</option>
+                    <option id="SedanPrisma" value="70">GM Prisma</option>
+                    <option id="SedanHB20S" value="70">Hyundai HB20S</option>
+                    <option id="PremiumCorolla" value="270">Toyota Corolla</option>
+                    <option id="PremiumCivic" value="270">Honda Civic</option>
+                    <option id="SuvTcross" value="200">VW T-Cross</option>
+                    <option id="SuvRenegade" value="200">Jeep Renegade</option>
+                </select>
 
-        <label id="labCustos">Se preferir, contrate um adicional</label>
-        <select id="custosAd">
-            <option value="0">Não, obrigado</option>
-            <option value="499">Proteção adicional para vidros (R$499,00)</option>
-            <option value="249">Assento para crianças (R$249,00)</option>
-            <option value="99">GPS (R$99,00)</option>
-        </select>
+                <label id="labHrRetirada">Selecione a hora da retirada</label>
+                <input type="time" id="horaRetirada"></input>
 
-        <label id="labDtRetirada">Selecione a data da retirada</label>
-        <input type="date" id="dataRetirada"></input>
+                <label id="labHrEntrega">Selecione a hora da entrega</label>
+                <input type="time" id="horaEntrega"></input>
 
-        <label id="labDtEntrega">Selecione a data da entrega</label>
-        <input type="date" id="dataEntrega" placeholder='Selecione a data de entrega'></input>
+                <label id="labValor">Valor/dia</label>
+                <input type="number" id="valorCategoria"></input>
+
+                <label id="labCustos">Se preferir, contrate um adicional</label>
+                <select id="custosAd">
+                    <option value="0">Não, obrigado</option>
+                    <option value="499">Proteção adicional para vidros (R$499,00)</option>
+                    <option value="249">Assento para crianças (R$249,00)</option>
+                    <option value="99">GPS (R$99,00)</option>
+                </select>
+
+                <label id="labDtRetirada">Selecione a data da retirada</label>
+                <input type="date" id="dataRetirada"></input>
+
+                <label id="labDtEntrega">Selecione a data da entrega</label>
+                <input type="date" id="dataEntrega" placeholder='Selecione a data de entrega'></input>
 
                 <h3 id="vlTotal">O valor total da(s) diária(s) é de R$ <span>{total}</span></h3>
-
+                
                 <div>
                     <button id="btnReservar" onClick={reservar}>Reservar</button>
                 </div>
-
+                <br />
                 <div>
                     <button id="calc" onClick={calculateTotal}>Calcular Total</button>
                 </div>
-            </section>
-            </div>
-        </>
-    );
+                <br />
+                <div>
+                    <label>Deseja limpar os campos? </label>
+                    <button id="btnRegistro" onClick={limparCampos}>Novo Registro</button>
+                </div>
+                <br />
+                <div>
+                    <label>Deseja excluir a reserva?</label>
+                    <button id="btnExcluir" onClick={excluir}>Excluir</button>
+                </div>
 
-    }
+            </section>
+        </>
+    )
+}
 export default Locacao;

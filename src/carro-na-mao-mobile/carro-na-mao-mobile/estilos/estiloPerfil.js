ba5ac60c75c8819583cc1b7ca0939999d8e42e8d
vitor hugo <vitorhugoemail@gmail.com>
2023-11-19 15:52:59 -0300

    arrumado metodo put do cadastro e adicionando mais alguns detalhes de estilo

--- src/carro-na-mao-mobile/carro-na-mao-mobile/estilos/estiloPerfil.js
+++ src/carro-na-mao-mobile/carro-na-mao-mobile/estilos/estiloPerfil.js
@@ -7,14 +7,15 @@ export default  StyleSheet.create({
     },
     email:{
         position:'relative',
-        top:-20,
+        top:-15,
         left:25
     },
     telefone:{
         position:'relative',
-        top:-20,
+        top:-15,
         left:25
     },
+
     body:{
         flex:1,
         backgroundColor:'#1F2024'
@@ -22,7 +23,9 @@ export default  StyleSheet.create({
     },
     apagar:{
        position:'relative',
-       top:190
+       left:290,
+       top:-60
+       
     },
     fotoUsuario:{
         position:'relative',
@@ -31,13 +34,14 @@ export default  StyleSheet.create({
     },
     dataNascimento:{
         position:'relative',
-        top:-20,
+        top:-15,
         left:25
     },
     card:{
         backgroundColor:'#8F9098',
         position:'relative',
-        top:50
+        top:50,
+        height:400
     },
     acessoRapido:{
         top:50,
@@ -45,5 +49,28 @@ export default  StyleSheet.create({
         color:'#fff',
         left:2,
         padding:24
+    },
+    iconeEditar:{
+        position:'relative',
+        left:0,
+        top:10
+    },
+    modal:{
+        backgroundColor:'#1F2024'
+      
+    },
+    input:{
+        backgroundColor:'#1F2024',
+        color:'#fff'
+    },
+    btnEditar:{
+        position:'relative',
+        top:50
+    },
+    close:{
+        position:'relative',
+        left:300
+        
     }
+    
   })
--- src/carro-na-mao-mobile/carro-na-mao-mobile/pages/paginas do usuario/perfil.js
+++ src/carro-na-mao-mobile/carro-na-mao-mobile/pages/paginas do usuario/perfil.js
@@ -1,7 +1,7 @@
 import{View,Alert} from "react-native"
 import axios from "axios";
 import React, {useState,useEffect} from "react";
-import { Card,Button,Avatar, Icon, MD3Colors,Divider,Modal,TextInput } from 'react-native-paper';
+import { Card,Button,Avatar, Icon, MD3Colors,Divider,Modal,TextInput, IconButton } from 'react-native-paper';
 import { RecuperaToken } from "../../Autenticação/autenticacao";
 import { useNavigation,useIsFocused,Link} from '@react-navigation/native';
 import {Text } from 'react-native-paper';
@@ -75,10 +75,10 @@ const Perfil = ()=> {
         setEndereco(dadosCompletos.endereco),
         setdtNasc(dadosCompletos.dataNacimento),
         setcatHab(dadosCompletos.categoriaHabilitacao),
-        setSenha(dadosCompletos.senha)};
+        setSenha(dadosCompletos.senha)
+    }
     const hideModal = () => setVisible(false);
     const [visible, setVisible] = React.useState(false);
-    
     const [email,setEmail] = useState()
     const [senha,setSenha] = useState()
     const [telefone,setTelefone] = useState()
@@ -90,7 +90,6 @@ const Perfil = ()=> {
 
     function editarDados(){
         const data = {
-            "id":dadosCompletos.id,
             "nome": dadosCompletos.nome,
             "dataNacimento": dtNasc,
             "endereco":endereceo,
@@ -102,19 +101,17 @@ const Perfil = ()=> {
             "categoriaHabilitacao":catHab,
             "usuarioAtivo": dadosCompletos.usuarioAtivo
          }
-    const headers ={
-      "Content-Type":"application/json",
-      "Authorization": 'Bearer ' + jwt
-     } 
-
-     axios.put('https://api-carronamao.azurewebsites.net/api/Cadastro/editar-usuario?id=+'+dadosCompletos.id+'',data,{headers}).then(response=>{
-        if(response.status==200){
-            alert('alterado com sucesso')
-            
-
+        const headers ={
+            "Content-Type":"application/json",
+            "Authorization": 'Bearer ' + jwt
+        } 
+        axios.put('https://api-carronamao.azurewebsites.net/api/Cadastro/editar-usuario?id='+dadosCompletos.id+'',data,{headers}).then(response=>{
+            if(response.status==200){
+             alert('alterado com sucesso')
             }
         }).catch(error=>{
             alert(error)
+            console.log(error)
         })
 
     }
@@ -126,119 +123,138 @@ const Perfil = ()=> {
                 <Card.Content type={'contained'}>
                     <Avatar.Icon style={estiloPerfil.fotoUsuario } color={'#fff'}size={80} icon="account" />
                     <Text style={estiloPerfil.title}> { dadosLocais.nome}</Text>
-                    <Icon source="email" color={'#fff'}size={20}/>
+                    <Text>{}</Text>
+                    <Icon source="email" color={'#fff'}size={20}  style={estiloPerfil.emailIcon}/>
                     <Text style={estiloPerfil.email}>{dadosLocais.email}</Text>
-                    <Icon source="phone" color={'#fff'} size={20}/>
+                    <Text>{}</Text>
+                    <Icon source="phone" color={'#fff'}  style={estiloPerfil.telefoneIcon} size={20}/>
                     <Text style={estiloPerfil.telefone}>{dadosLocais.telefone}</Text>
-                    <Icon source="calendar-month-outline"color={'#fff'} size={20}/>
+                    <Text>{}</Text>
+                    <Icon source="calendar-month-outline"color={'#fff'} size={20} style={estiloPerfil.datanasIcon}/>
                     <Text style={estiloPerfil.dataNascimento}>{dadosLocais.dataNascimento}</Text>
-                    <Button onPress={showModal}>Show</Button>
+                    <Text>{}</Text>
+                    <Icon source="card-account-details-outline"color={'#fff'} size={20} style={estiloPerfil.datanasIcon}/>
+                    <Text style={estiloPerfil.dataNascimento}>{dadosCompletos.categoriaHabilitacao}</Text>
+                    <IconButton onPress={()=>showModal()} icon="square-edit-outline"  size={40} iconColor={'#fff'} style={estiloPerfil.iconeEditar}/>
+                    <IconButton onPress={()=>confirmarExclusão()} icon="delete-outline"  size={40} iconColor={'red'} style={estiloPerfil.apagar}/>
                 </Card.Content>
             </Card>
+
             <View style={estiloPerfil.acessoRapido}>
                 <Text style={{color:'#fff',fontSize:26}}>Acesso Rápido</Text>
                 <Text>{'/n'}</Text> 
                 <Link  style={{color:'#fff'}} to='/Avaliacao'>Avaliações</Link> 
-                <Text>{'/n'}</Text>
                 <Divider />
                 <Text>{'/n'}</Text>
-                <Link  style={{color:'#fff'}} to='/cadastrarVistoria'>Vistorias</Link>      
+                <Link  style={{color:'#fff'}} to='/cadastrarVistoria'>Vistorias</Link>
+                <Divider />
+                <Text>{'/n'}</Text>
+                <Link  style={{color:'#fff'}} to='/login'>Sair do APP</Link>      
             </View>
-            <Modal visible={visible} onDismiss={hideModal}>
-            <TextInput
-                    mode='outlined'
-                    label="Email"
-                    keyboardType={'email-address'}
-                    placeholder="Digite seu email"
-                    value={email}
-                    onChangeText={email=>setEmail(email)} 
-                   
-                    activeOutlineColor="#fff"
-                    textColor="#000"
-                    underlineColor="#fff"
-                    outlineColor="#fff"
-            />
-            <TextInput
-                    mode='outlined'
-                    label="Senha"
-                    placeholder="Digite sua senha"
-                    value={senha}
-                    onChangeText={senha => setSenha(senha)}
-                    secureTextEntry={true}
-                    activeOutlineColor="#fff"
-                    textColor="#000"
-                    underlineColor="#fff"
-                    outlineColor="#fff"
-                />
+
+            <Modal visible={visible} onDismiss={hideModal} style={estiloPerfil.modal}>
+            <Icon source="close"color={'#fff'} size={20} style={estiloPerfil.close}/>
                 <TextInput
-                    mode='outlined'
-                    label="Telefone"
-                    keyboardType={'email-address'}
-                    placeholder="Digite seu email"
-                    value={telefone}
-                    onChangeText={telefone=>setTelefone(telefone)} 
-        
-                    activeOutlineColor="#fff"
-                    textColor="#000"
-                    underlineColor="#fff"
-                    outlineColor="#fff"
-            />
-              <TextInput
-                    mode='outlined'
-                    label="CPF"
-                    keyboardType={'email-address'}
-                    placeholder="Digite seu email"
-                    value={cpf}
-                    onChangeText={cpf=>setCPF(cpf)} 
-        
-                    activeOutlineColor="#fff"
-                    textColor="#000"
-                    underlineColor="#fff"
-                    outlineColor="#fff"
-            />
-              <TextInput
-                    mode='outlined'
-                    label="Endereço"
-                    keyboardType={'email-address'}
-                    placeholder="Digite seu email"
-                    value={endereceo}
-                    onChangeText={endereceo=>setEndereco(endereceo)} 
-        
-                    activeOutlineColor="#fff"
-                    textColor="#000"
-                    underlineColor="#fff"
-                    outlineColor="#fff"
-            />
-              <TextInput
-                    mode='outlined'
-                    label="Data de nascimento"
-                    keyboardType={'email-address'}
-                    placeholder="Digite seu email"
-                    value={dtNasc}
-                    onChangeText={dtNasc=>setdtNasc(dtNasc)} 
-        
-                    activeOutlineColor="#fff"
-                    textColor="#000"
-                    underlineColor="#fff"
-                    outlineColor="#fff"
-            />
-              <TextInput
-                    mode='outlined'
-                    label="categoria habilitação"
-                    keyboardType={'email-address'}
-                    placeholder="Digite seu email"
-                    value={catHab}
-                    onChangeText={catHab=>setcatHab(catHab)} 
-        
-                    activeOutlineColor="#fff"
-                    textColor="#000"
-                    underlineColor="#fff"
-                    outlineColor="#fff"
-            />
-                    
-                 <Button onPress={()=>editarDados()}>Editar</Button>
+                        mode='outlined'
+                        label="Email"
+                        keyboardType={'email-address'}
+                        placeholder="Digite seu email"
+                        value={email}
+                        onChangeText={email=>setEmail(email)} 
+                        style={estiloPerfil.input}
+                        activeOutlineColor="#fff"
+                        textColor="#fff"
+                        underlineColor="#fff"
+                        outlineColor="#fff"
+                />
+                 <Text>{'/n'}</Text>
+                <TextInput
+                        mode='outlined'
+                        label="Senha"
+                        placeholder="Digite sua senha"
+                        value={senha}
+                        onChangeText={senha => setSenha(senha)}
+                        style={estiloPerfil.input}
+                        secureTextEntry={true}
+                        activeOutlineColor="#fff"
+                        textColor="#fff"
+                        underlineColor="#fff"
+                        outlineColor="#fff"
+                    />
+                     <Text>{'/n'}</Text>
+                    <TextInput
+                        mode='outlined'
+                        label="Telefone"
+                        keyboardType={'email-address'}
+                        placeholder="Digite seu email"
+                        value={telefone}
+                        onChangeText={telefone=>setTelefone(telefone)} 
+                        style={estiloPerfil.input}
+                        activeOutlineColor="#fff"
+                        textColor="#fff"
+                        underlineColor="#fff"
+                        outlineColor="#fff"
+                />
+                 <Text>{'/n'}</Text>
+                <TextInput
+                        mode='outlined'
+                        label="CPF"
+                        keyboardType={'email-address'}
+                        placeholder="Digite seu email"
+                        value={cpf}
+                        onChangeText={cpf=>setCPF(cpf)} 
+                        style={estiloPerfil.input}
+                        activeOutlineColor="#fff"
+                        textColor="#fff"
+                        underlineColor="#fff"
+                        outlineColor="#fff"
+                />
+                 <Text>{'/n'}</Text>
+                <TextInput
+                        mode='outlined'
+                        label="Endereço"
+                        keyboardType={'email-address'}
+                        placeholder="Digite seu email"
+                        value={endereceo}
+                        onChangeText={endereceo=>setEndereco(endereceo)} 
+                        style={estiloPerfil.input}
+                        activeOutlineColor="#fff"
+                        textColor="#fff"
+                        underlineColor="#fff"
+                        outlineColor="#fff"
+                />
+                 <Text>{'/n'}</Text>
+                <TextInput
+                        mode='outlined'
+                        label="Data de nascimento"
+                        keyboardType={'email-address'}
+                        placeholder="Digite seu email"
+                        value={dtNasc}
+                        onChangeText={dtNasc=>setdtNasc(dtNasc)} 
+                        style={estiloPerfil.input}
+                        activeOutlineColor="#fff"
+                        textColor="#fff"
+                        underlineColor="#fff"
+                        outlineColor="#fff"
+                />
+                 <Text>{'/n'}</Text>
+                <TextInput
+                        mode='outlined'
+                        label="categoria habilitação"
+                        keyboardType={'email-address'}
+                        placeholder="Digite seu email"
+                        value={catHab}
+                        onChangeText={catHab=>setcatHab(catHab)} 
+                        style={estiloPerfil.input}
+                        activeOutlineColor="#fff"
+                        textColor="#fff"
+                        underlineColor="#fff"
+                        outlineColor="#fff"
+                />
+                        
+                 <Button   style={estiloPerfil.btnEditar} onPress={()=>editarDados()}  textColor={'green'}>Editar</Button>
             </Modal>
-            <Button style={estiloPerfil.apagar} onPress={()=>confirmarExclusão()} textColor={'red'} title="Deletar minha conta">Deletar minha conta</Button>
+            
         </View>
     )
 }

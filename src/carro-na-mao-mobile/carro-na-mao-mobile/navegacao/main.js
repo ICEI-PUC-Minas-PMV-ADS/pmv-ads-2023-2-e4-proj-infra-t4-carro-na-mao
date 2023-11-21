98d332d3d4206c556cbe5427884109c4e32d925b
mthszf99 <101369477+mthszf99@users.noreply.github.com>
2023-11-20 22:56:52 -0300

    update main

--- src/carro-na-mao-mobile/carro-na-mao-mobile/navegacao/main.js
+++ src/carro-na-mao-mobile/carro-na-mao-mobile/navegacao/main.js
@@ -2,39 +2,45 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import login from '../pages/paginas do usuario/login'
 import Avaliacao from '../pages/avaliacoes/avaliacao'
-
 import Cadastro from '../pages/paginas do usuario/cadastro'
-import viewLocacao from '../pages/locacao/viewLocacao'
-import cadastrarLocacao from '../pages/locacao/cadastrarLocacao'
 import cadastrarAvaliacao from '../pages/avaliacoes/cadastrarAvaliacao'
 import Perfil from '../pages/paginas do usuario/perfil'
-import menu from '../pages/menu'
+import Menu from '../pages/menu'
+import viewLocacao from '../pages/locacao/viewLocacao'
+import cadastrarLocacao from '../pages/locacao/cadastrarLocacao'
 import avaliacaoUsaurios from '../pages/avaliacoes/avaliacoesEspcificasPorusuario'
 import cadastrarVistoria from '../pages/vistorias/cadastrarVistoria'
+import menu from '../pages/menu'
 //import Cadastro from '../pages/cadastro'
 const stack = createNativeStackNavigator()
 
-const Main = () => {
+function Main() {
     return (
         <NavigationContainer independent={true}>
-            <stack.Navigator>
+            <stack.Navigator
+                initialRouteName='login'
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#1F2024', // Substitua 'blue' pela cor desejada
+                    },
+                    headerTintColor: 'white', // Cor do texto do cabeçalho
+                }}
+            >
                 <stack.Screen
                     name='login'
                     component={login}
                     options={{ header: () => null }}
                 />
+
                 <stack.Screen
                     name='Cadastro'
                     component={Cadastro}
                     options={{ header: () => null }}
                 />
                 <stack.Screen
-                    name='Avalicao'
-                    component={Avaliacao}
-                />
-                <stack.Screen
-                    name='cadastrarAvaliacao'
-                    component={cadastrarAvaliacao}
+                    name='menu'
+                    component={Menu}
+                    options={{ header: () => null }}
                 />
                 <stack.Screen
                     name='viewLocacao'
@@ -45,17 +51,25 @@ const Main = () => {
                     component={cadastrarLocacao}
                 />
                 <stack.Screen
+                    name='Avaliacao'
+                    component={Avaliacao}
+                    options={{ title: 'Avaliações' }}
+                />
+                <stack.Screen
+                    name='cadastrarAvaliacao'
+                    component={cadastrarAvaliacao}
+                    options={{ title: 'Adicionar nova avaliação' }}
+                />
+                <stack.Screen
                     name='cadastrarVistoria'
                     component={cadastrarVistoria}
+                    options={{ title: 'Registro de Vistoria' }}
                 />
                 <stack.Screen
                     name='Perfil'
                     component={Perfil}
                 />
-                <stack.Screen
-                    name='avaliacaoUsaurios'
-                    component={avaliacaoUsaurios}
-                />
+
 
             </stack.Navigator>
         </NavigationContainer>

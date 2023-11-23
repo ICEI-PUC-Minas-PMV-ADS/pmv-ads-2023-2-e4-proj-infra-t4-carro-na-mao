136da4eebba959f640f47583ac462826fe3fa5e9
Matheus Sampaio <matheuslemos98@gmail.com>
2023-11-22 21:06:04 -0300

    Ajusta cor de fundo

--- src/carro-na-mao-mobile/carro-na-mao-mobile/estilos/estiloLocalizacao.js
+++ src/carro-na-mao-mobile/carro-na-mao-mobile/estilos/estiloLocalizacao.js
@@ -3,7 +3,7 @@ import {StyleSheet } from "react-native";
 export default StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '##1F2024',
       alignItems: 'center',
       justifyContent: 'center',
     },

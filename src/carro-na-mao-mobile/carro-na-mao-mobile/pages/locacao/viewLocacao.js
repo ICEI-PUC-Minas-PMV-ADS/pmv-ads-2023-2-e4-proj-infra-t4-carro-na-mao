f630b195b2033005b071758042b723a725ad7b29
mthszf99 <101369477+mthszf99@users.noreply.github.com>
2023-11-24 21:10:48 -0300

    delete funcionando

--- src/carro-na-mao-mobile/carro-na-mao-mobile/pages/locacao/viewLocacao.js
+++ src/carro-na-mao-mobile/carro-na-mao-mobile/pages/locacao/viewLocacao.js
@@ -43,89 +43,77 @@ const Locacao = () => {
 
   const handleDelete = async (id_locacao) => {
     console.log('ID da reserva a ser excluída:', id_locacao);
-    try {
-      const jwtToken = await RecuperaToken();
+    const jwtToken = await RecuperaToken();
 
-      Alert.alert(
-        'Confirmar exclusão',
-        'Deseja realmente excluir esta reserva?',
-        [
-          {
-            text: 'Cancelar',
-            style: 'cancel',
-          },
-          {
-            text: 'Confirmar',
-            onPress: async () => {
-              const headers = {
-                "Content-Type": "application/json",
-                "Authorization": 'Bearer ' + jwtToken
-              };
+    Alert.alert(
+      'Confirmar exclusão',
+      'Deseja realmente excluir esta reserva?',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Confirmar',
+          onPress: async () => {
+            const headers = {
+              "Content-Type": "application/json",
+              "Authorization": 'Bearer ' + jwtToken
+            }
+            axios.delete('https://api-carronamao.azurewebsites.net/api/Locacao?id_locacao=' + id_locacao + '', { headers })
+              .then(response => {
+                if (response.status == 200) {
+                  alert('deletada com sucesso')
 
-              try {
-                const response = await axios.delete(`https://api-carronamao.azurewebsites.net/api/Locacao/${id_locacao}`, { headers });
-
-                if (response.status === 200) {
-                  alert('Reserva excluída com sucesso');
-                  navigation.navigate('viewLocacao');
                 }
-              } catch (error) {
-                if (error.response && error.response.status === 400) {
-                  console.error('Erro 404 ao excluir reserva:', error.response.data);
-                  alert('Erro 404 ao excluir reserva: ' + error.response.data.message);
-                } else {
-                console.error('Erro ao excluir reserva:', error);
-                alert('Erro ao excluir reserva' + error.message);
-                }
-              }
-            },
-          },
-        ],
-        { cancelable: false }
-      );
-    } catch (error) {
-      console.error('Erro ao recuperar token:', error);
-    }
-  };
+              }).catch(error => {
+                alert(error)
+              })
+          }
+        }
+      ])
+  }
 
-  const Item = ({ item }) => {
-    return (
-      <TouchableOpacity onPress={() => navigation.navigate('editarLocacao', { id: item.id_locacao })} >
-        
-        <View style={estiloLocacao.informacoe}>
-          <Text>{'Local: ' + item.id_local}</Text>
-          <Text>{'Categoria: ' + item.id_categoria}</Text>
-          <Text>{'Modelo Veículo: ' + item.modelo_veiculo}</Text>
-          <Text>{'Hora Retirada: ' + item.hora_retirada}</Text>
-          <Text>{'Data Retirada: ' + format(new Date(item.data_retirada), 'dd/MM/yyyy')}</Text>
-          <Text>{'Valor da diaria: ' + item.vl_categoria}</Text>
-          <IconButton
-            icon="delete"
-            style={estiloLocacao.exclui}
-            iconColor='#8B0000'
-            onPress={() => handleDelete(item.id_locacao)}
-          />
-        </View>
-      </TouchableOpacity>
-    );
-  };
 
+const Item = ({ item }) => {
   return (
-    <View style={estiloLocacao.body}>
-      <FlatList
-        style={estiloLocacao.topicos}
-        data={locacao}
-        renderItem={Item}
-        keyExtractor={item => (item.id ? item.id.toString() : null)}
-      />
+    <TouchableOpacity onPress={() => navigation.navigate('editarLocacao', { id: item.id_locacao })} >
 
-      <FAB
-        style={estiloLocacao.adic}
-        icon="plus"
-        onPress={() => navigation.navigate('cadastrarLocacao')}
-      />
-    </View>
+      <View style={estiloLocacao.informacoe}>
+        <Text>{'Local: ' + item.id_local}</Text>
+        <Text>{'Categoria: ' + item.id_categoria}</Text>
+        <Text>{'Modelo Veículo: ' + item.modelo_veiculo}</Text>
+        <Text>{'Hora Retirada: ' + item.hora_retirada}</Text>
+        <Text>{'Data Retirada: ' + format(new Date(item.data_retirada), 'dd/MM/yyyy')}</Text>
+        <Text>{'Valor da diaria: ' + item.vl_categoria}</Text>
+        <IconButton
+          icon="delete"
+          style={estiloLocacao.exclui}
+          iconColor='#8B0000'
+          onPress={() => handleDelete(item.id_locacao)}
+        />
+      </View>
+    </TouchableOpacity>
   );
+};
+
+return (
+  <View style={estiloLocacao.body}>
+    <FlatList
+      style={estiloLocacao.topicos}
+      data={locacao}
+      renderItem={Item}
+      keyExtractor={item => (item.id_locacao ? item.id_locacao.toString() : null)}
+    />
+
+    <FAB
+      style={estiloLocacao.adic}
+      icon="plus"
+      onPress={() => navigation.navigate('cadastrarLocacao')}
+    />
+  </View>
+);
 }
+
 
 export default Locacao;

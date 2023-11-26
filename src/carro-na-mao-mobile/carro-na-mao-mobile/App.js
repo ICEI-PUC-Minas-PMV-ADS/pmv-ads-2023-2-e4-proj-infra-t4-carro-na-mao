1e746e8c3cc09302d0c6568bdd84078a8fc7ec48
rogerpucmg <101529285+rogerpucmg@users.noreply.github.com>
2023-11-26 08:17:51 -0300

    api notificacoes push
    componente para push notificacoes + doc plano de teste

--- src/carro-na-mao-mobile/carro-na-mao-mobile/App.js
+++ src/carro-na-mao-mobile/carro-na-mao-mobile/App.js
@@ -6,11 +6,13 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Main from "./navegacao/main";
 import  Menu  from "./pages/menu";
 import Login from "././pages/paginas do usuario/login";
+import registerNNPushToken from 'native-notify';
 
 const Stack = createStackNavigator();
 
 
 export default function App() {
+  registerNNPushToken(15111, 'yM7rxy4aracvZo2dZmI275');
 
   return (
     <NavigationContainer>

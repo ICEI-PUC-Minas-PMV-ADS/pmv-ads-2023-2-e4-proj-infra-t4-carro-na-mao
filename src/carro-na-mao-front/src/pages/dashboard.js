2deb3c95582a591cbc89a2d171a16ade87eeebdd
Pedro Almeida <101743181+Pedroassis1707@users.noreply.github.com>
2023-11-26 22:06:02 -0300

    css e ajustes finais

--- src/carro-na-mao-front/src/pages/dashboard.js
+++ src/carro-na-mao-front/src/pages/dashboard.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import{Menu} from './menu';
+import '../estilos/dashboard.css'
+
 
 function Dashboard(){
 
     return(
-        <>
+        <div id="fundoDashboard">
         <Menu/>
-        <h1>Dashboard</h1>
-        <iframe title="dashboard-carro-na-mao" width="1900" height="760" src="https://app.powerbi.com/view?r=eyJrIjoiNDVmMjQ3ZDMtODM3Ny00NDI0LTkwMzktODhhN2YwYjc4NjUyIiwidCI6IjE0Y2JkNWE3LWVjOTQtNDZiYS1iMzE0LWNjMGZjOTcyYTE2MSIsImMiOjh9" frameborder="0" allowFullScreen="true"></iframe>
-        </>
+        <iframe title="dashboard-carro-na-mao" width="1360" height="548" src="https://app.powerbi.com/view?r=eyJrIjoiNDVmMjQ3ZDMtODM3Ny00NDI0LTkwMzktODhhN2YwYjc4NjUyIiwidCI6IjE0Y2JkNWE3LWVjOTQtNDZiYS1iMzE0LWNjMGZjOTcyYTE2MSIsImMiOjh9" frameborder="0" allowFullScreen="true"></iframe>
+        </div>
     );
 }
 
